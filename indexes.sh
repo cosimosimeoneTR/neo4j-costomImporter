a71cca5b03cdd6ac36a63b3868d07580a3583f7e
Cosimo Simeone <cosimo.simeone@thomsonreuters.com>
2016-09-05 11:32:04 +0200

    Cypher file parameter is now optional; if none passed, `basename $0`.sh.cy will be executed

--- indexes.sh
+++ indexes.sh
@@ -2,25 +2,25 @@
 
 set +x
 
+. /home/neo4j/.bash_profile
+. /opt/reuters/apps/env/neo4j.env
+
 shopt -s expand_aliases
 alias echoi='echo `date +%Y-%m-%d\ %H:%M.%S` -INFO-'
 alias echoe='echo `date +%Y-%m-%d\ %H:%M.%S` -ERROR-'
 
 echoi STARTing
 
-if [ $# -ne 1 ]
-  then
-    echoe "Please pass script file name"
-    exit 1
-fi
 outFile=/tmp/`basename $0`.out
 
+if [ $# -eq 1 ]
+then
+  cyptherFile=$1
+else
+  cyptherFile=`basename $0`.sh.py
+fi
 
-. /home/neo4j/.bash_profile
-. /opt/reuters/apps/env/neo4j.env
-
-
-echoi Executing script in $1
+echoi Executing script file $cyptherFile
 echoi Logging output in $outFile
 
 $NEO4J_HOME/bin/neo4j-shell -file "$1" >$outFile 2>&1
